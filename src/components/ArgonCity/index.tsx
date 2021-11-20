4c164a7e981c6850aa621252969ea0c9e329a412
Aakash Mallik <humble_d@grape.shot>
2021-11-20 23:12:50 +0530

    Initial commit

---
+++ src/components/ArgonCity/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ArgonCity(): JSX.Element {
+  return <div>Hello sir</div>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import './global.css';
+import React from 'react';
+import { render } from 'react-dom';
+import ArgonCity from './components/ArgonCity';
+
+render(<ArgonCity />, document.querySelector('#root'));
